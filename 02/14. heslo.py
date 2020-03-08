9bb2e4e27164b4c1d2e99bd53b3791bbb0ab67af
PavlaSva <61755638+PavlaSva@users.noreply.github.com>
2020-03-08 23:42:00 +0100

    Delete 14. heslo.py

--- 02/14. heslo.py
+++
@@ -1,9 +0,0 @@
-heslo = input('Zadej heslo: ')
-heslo_je_spravne = heslo == "333"
-
-if heslo_je_spravne:
-    print('Odpověď na základní otázku života, vesmíru a vůbec je 42')
-    print('"Stopařův průvodce po galaxii"')
-else:
-    print ("Heslo jsi neuhodl, zkus to znova")
-
